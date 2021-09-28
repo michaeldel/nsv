1a4d7a3b650f585f7d3cf64c6f55212caab50207
michaeldel <michaeldel@protonmail.com>
2021-09-28 05:03:29 +0200

    Add texture initialization and event loop

--- nsv.c
+++ nsv.c
@@ -25,7 +25,53 @@ int main(void) {
         return EXIT_FAILURE; /* TODO: UNIX exit codes */
     }
 
+    SDL_Renderer * renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+
+    if (renderer == NULL) {
+        fprintf(stderr, "Unable to create renderer: %s\n", SDL_GetError());
+        return EXIT_FAILURE; /* TODO: UNIX exit codes */
+    }
+
+    const int width = INITIAL_WIDTH;
+    const int height = INITIAL_HEIGHT;
+    const Uint32 pixelformat = SDL_PIXELFORMAT_RGBA8888;
+
+    SDL_Texture * texture = SDL_CreateTexture(
+        renderer,
+        pixelformat,
+        SDL_TEXTUREACCESS_STREAMING,
+        width,
+        height
+    );
+
+    if (texture == NULL) {
+        fprintf(stderr, "Unable to create texture: %s\n", SDL_GetError());
+        return EXIT_FAILURE; /* TODO: UNIX exit codes */
+    }
+
+    SDL_PixelFormat * format = SDL_AllocFormat(pixelformat);
+
+    if (format == NULL) {
+        fprintf(stderr, "Unable to allocate format: %s\n", SDL_GetError());
+        return EXIT_FAILURE; /* TODO: UNIX exit codes */
+    }
+
+    for (;;) {
+        SDL_Event event;
+        SDL_PollEvent(&event);
+
+        if (event.type == SDL_QUIT) break;
+
+        SDL_RenderClear(renderer);
+        SDL_RenderPresent(renderer);
+    }
+
+    SDL_FreeFormat(format);
+
+    SDL_DestroyTexture(texture);
+    SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
+
     SDL_Quit();
 
     return EXIT_SUCCESS;
