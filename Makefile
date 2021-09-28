fbc66d4b8d7100a33a2d492a0678c12c76dce116
michaeldel <michaeldel@protonmail.com>
2021-09-28 04:51:17 +0200

    Add SDL hello world

--- Makefile
+++ Makefile
@@ -1,8 +1,9 @@
 CC = gcc
 CFLAGS = -Wall -Wextra -Werror -pedantic
+LDFLAGS = -lSDL2
 
 nsv: nsv.c
-	$(CC) $(CFLAGS) -o $@ $^
+	$(CC) $(CFLAGS) -o $@ $(LDFLAGS) $^
 
 clean:
 	rm nsv
--- nsv.c
+++ nsv.c
@@ -1,7 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include <SDL2/SDL.h>
+
+#define INITIAL_WIDTH 640
+#define INITIAL_HEIGHT 320
+
 int main(void) {
-    printf("Hello world!\n");
+    int err = SDL_Init(SDL_INIT_VIDEO);
+    if (err) {
+        fprintf(stderr, "Unable to initialize SDL: %s\n", SDL_GetError());
+        return EXIT_FAILURE; /* TODO: UNIX exit codes */
+    }
+
+    SDL_Window * window = SDL_CreateWindow(
+        "nsv",
+        SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+        INITIAL_WIDTH, INITIAL_HEIGHT,
+        SDL_WINDOW_OPENGL
+    );
+
+    if (window == NULL) {
+        fprintf(stderr, "Unable to create window: %s\n", SDL_GetError());
+        return EXIT_FAILURE; /* TODO: UNIX exit codes */
+    }
+
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
     return EXIT_SUCCESS;
 }
