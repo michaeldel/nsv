2d4325e5238624e554e6de0dbfd2a7e9a2f88540
michaeldel <michaeldel@protonmail.com>
2021-10-15 05:39:35 +0200

    Add rendering for different bases

--- textures.c
+++ textures.c
@@ -12,7 +12,7 @@ void filltexturewithsequence(
     const struct sequence * sequence,
     uint8_t base
 ) {
-    assert(base == 2); /* TODO: more bases */
+    assert(base >= 1);
 
     int w, h, access;
     SDL_QueryTexture(texture, NULL, &access, &w, &h);
@@ -29,14 +29,28 @@ void filltexturewithsequence(
 
     for (size_t y = 0; y < height; y++) 
         for (size_t x = 0; x < width; x++) {
-            mpz_t num;
-            mpz_init_set(num, sequence->numbers[x]);
-            const mp_bitcnt_t bitindex = height - y - 1;
-            const unsigned int ison = mpz_tstbit(num, bitindex);
+            unsigned int digit;
 
-            const Uint8 r = ison * 255;
-            const Uint8 g = ison * 255;
-            const Uint8 b = ison * 255;
+            if (base != 2) {
+                const unsigned int digitindex = height - y - 1;
+
+                mpz_t powofbase, quotient;
+                mpz_inits(powofbase, quotient, NULL);
+
+                mpz_ui_pow_ui(powofbase, base, digitindex);
+                mpz_fdiv_q(quotient, sequence->numbers[x], powofbase);
+
+                digit = mpz_mod_ui(quotient, quotient, base);
+                mpz_clears(powofbase, quotient);
+            } else {
+                const mp_bitcnt_t bitindex = height - y - 1;
+                digit = mpz_tstbit(sequence->numbers[x], bitindex);
+            }
+
+            /* TODO: color map */
+            const Uint8 r = digit * 255 / (base - 1);
+            const Uint8 g = digit * 255 / (base - 1);
+            const Uint8 b = digit * 255 / (base - 1);
 
             pixels[y * width + x] = SDL_MapRGB(format, r, g, b);
         }
