2d436cfa5967f5d3717e46db972f7902a50e6489
michaeldel <michaeldel@protonmail.com>
2021-10-16 08:24:56 +0200

    Add simple sequence rendering

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,59 @@
+extern crate piston;
+extern crate glutin_window;
+extern crate graphics;
+extern crate opengl_graphics;
+
+use graphics::Transformed;
+use glutin_window::GlutinWindow;
+use opengl_graphics::{Filter, OpenGL, GlGraphics, Texture, TextureSettings};
+use piston::event_loop::{Events, EventLoop, EventSettings};
+use piston::input::RenderEvent;
+use piston::window::WindowSettings;
+
+const WINDOW_WIDTH: u32 = 640;
+const WINDOW_HEIGHT: u32 = 320;
+
 fn main() {
-    println!("Hello, world!");
+    let opengl = OpenGL::V3_2;
+    let settings = WindowSettings::new("nsv", [WINDOW_WIDTH, WINDOW_HEIGHT])
+        .graphics_api(opengl)
+        .exit_on_esc(true);
+    let mut window: GlutinWindow = settings.build()
+        .expect("Could not create window");
+
+    let sequence = 0..1024;
+
+    let mut events = Events::new(EventSettings::new().lazy(true));
+    let mut gl = GlGraphics::new(opengl);
+
+    let width = sequence.len();
+    let height = 32;
+    let mut buf = vec![0; width * height];
+
+    for (x, number) in sequence.enumerate() {
+        for y in 0..height {
+            let ison = (number >> (height - y - 1) & 1) as u8;
+            buf[x + width * y] = ison * 255;
+        }
+    }
+
+    let image = graphics::Image::new().rect([0f64, 0f64, width as f64, height as f64]);
+    let texture_settings = TextureSettings::new().filter(Filter::Nearest);
+    let texture = Texture::from_memory_alpha(
+        &buf, width as u32, height as u32, &texture_settings
+    ).unwrap();
+
+    let zoom = 4.0;
+
+    while let Some(e) = events.next(&mut window) {
+        if let Some(args) = e.render_args() {
+            gl.draw(args.viewport(), |c, g| {
+                graphics::clear([0.0; 4], g);
+                let zoomed = c.transform.zoom(zoom);
+                image.draw(
+                    &texture, &graphics::draw_state::DrawState::default(), zoomed, g
+                );
+            });
+        }
+    }
 }
